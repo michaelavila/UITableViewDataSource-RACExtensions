54edd6a7b17880a5c4d45018c816443741266529
Michael Avila <me@michaelavila.com>
2014-06-16 07:49:10 +0000

    Remove unnecessary code

--- Examples/Basic/Example/ETableViewController.m
+++ Examples/Basic/Example/ETableViewController.m
@@ -8,22 +8,6 @@
 
 #import "ETableViewController.h"
 
-@interface ETableViewController ()
-
-@end
-
 @implementation ETableViewController
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
